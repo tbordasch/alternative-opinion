957bc862cab4930e1f9c48d8a9121973d0aca365
tbordasch <tb.bordasch@gmail.com>
2025-12-15 22:02:22 +0100

    Fix layout: Landing page fills viewport (comments+opinion), footer properly positioned on all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <ThemeProvider>
           <div className="flex flex-col min-h-screen">
             <Navbar />
-            <div className="flex-1 min-h-0">
+            <div className="flex-1">
               {children}
             </div>
           </div>
